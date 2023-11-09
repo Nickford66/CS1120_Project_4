a0161ef2d21f6bd427f2b68d18f933068d0c4ca6
Nickford66 <143648965+Nickford66@users.noreply.github.com>
2023-11-09 18:22:24 -0500

    Update and rename testbookshop to testbookshop.py

---
+++ testbookshop.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+class TestBookshop(TestCase):
+  def __init__(self):
+    super().__init__(self)
+  def test_01(self):
+  def test_02(self):
+  def test_03(self):
+  def test_04(self):
+  def test_05(self):
+  def test_06(self):
+  def test_07(self):
+  def test_08(self):
+  def test_09(self):
+  def test_10(self):
+  
+
